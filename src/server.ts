8ea32af97bca2f20402fa452df566f473bbdcf74
Divyansh Bhardwaj <divyanshb22@gmail.com>
2025-12-22 11:38:03 +0530

    update server configuration and TypeScript settings; add .env support and modify .gitignore

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.listen(
+	PORT,
+	() => {
+		console.log(`server started on port: ${PORT}`)
+	}
+);
+
